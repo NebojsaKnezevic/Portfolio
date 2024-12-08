ffaf3205d92337d46727e1d7771c76e0f7b0444b
Nebojsa Knezevic <global021man@gmail.com>
2024-12-08 17:02:36 +0100

    Layout postavljen.

---
+++ src/components/layout/index.tsx
@@ -0,0 +1,12 @@
+import { Box } from "@mui/material";
+
+interface ILayoutProps {
+    children?: React.ReactNode;
+  }
+
+export const Layout: React.FC<ILayoutProps> = ({ children }) => {
+
+    return (
+        <Box sx={{color: 'white'}}>fdsfasgfasdgfsdgdfg</Box>
+    );
+}
